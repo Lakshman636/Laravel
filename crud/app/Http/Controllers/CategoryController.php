edffe675dc5318543d189b8655e2284c63539e3f
Lakshman636 <lakshmanudaya79@gmail.com>
2025-08-15 02:03:37 +0530

    complete the show data function

--- crud/app/Http/Controllers/CategoryController.php
+++ crud/app/Http/Controllers/CategoryController.php
@@ -14,7 +14,10 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('category.index');
+        $categories = Category::paginate(10);
+        return view('category.index',[
+            'categories' => $categories
+        ]);
     }
 
     /**
