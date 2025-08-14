0b9bfe55b80a3ae632005b30f178277b4015f2e7
Lakshman636 <lakshmanudaya79@gmail.com>
2025-08-15 00:20:47 +0530

    set up the migration and models

--- crud/database/migrations/2025_08_14_183808_create_categories_table.php
+++ crud/database/migrations/2025_08_14_183808_create_categories_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->boolean('status')->default(1)-> comment('1=visible,0=hidden');
             $table->timestamps();
         });
     }
