f7fe06c948fdde20b589f88914e8474a8f49f47c
Lakshman636 <lakshmanudaya79@gmail.com>
2025-08-15 02:31:41 +0530

    complete show category

--- crud/app/Http/Controllers/CategoryController.php
+++ crud/app/Http/Controllers/CategoryController.php
@@ -60,7 +60,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        return view('category.show');
+        return view('category.show',compact('category'));
     }
 
     /**
